03284eb1077dbe258e167d85ad56c721000bf781
gilgeunwoo <kesksjxhx@gmail.com>
2024-01-27 16:28:19 +0900

    ,

--- src/main/kotlin/com/gil/impacthon1st_backend/Impacthon1stBackendApplication.kt
+++ src/main/kotlin/com/gil/impacthon1st_backend/Impacthon1stBackendApplication.kt
@@ -1,8 +1,10 @@
 package com.gil.impacthon1st_backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class Impacthon1stBackendApplication
 
